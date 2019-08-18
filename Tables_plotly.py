e9976cbe8c52d4feaf4c5b87278eca0c07c812e3
piokowalski <piokowalski@onet.pl>
2019-08-18 14:54:41 +0200

    Tables - basic table styled

--- Tables_plotly.py
+++ Tables_plotly.py
@@ -4,3 +4,22 @@ fig1 = go.Figure(data=[go.Table(header=dict(values=['A Scores', 'B Scores']),
                  cells=dict(values=[[100, 90, 80, 90], [95, 85, 75, 95]]))
                      ])
 fig1.show()
+
+#
+## Styled Table
+#
+
+fig2 = go.Figure(data=[go.Table(
+    header=dict(values=['A Scores', 'B Scores'],
+                line_color='darkslategray',
+                fill_color='lightskyblue',
+                align='left'),
+    cells=dict(values=[[100, 90, 80, 90], # 1st column
+                       [95, 85, 75, 95]], # 2nd column
+               line_color='darkslategray',
+               fill_color='lightcyan',
+               align='left'))
+])
+
+fig2.update_layout(width=500, height=300)
+fig2.show()
