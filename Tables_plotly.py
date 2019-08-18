2fe960421a1718afce01a6c8d3e9c7ff3e330108
piokowalski <piokowalski@onet.pl>
2019-08-18 15:04:50 +0200

    Pandas Dataframe library (SSL issues)

--- Tables_plotly.py
+++ Tables_plotly.py
@@ -23,3 +23,22 @@ fig2 = go.Figure(data=[go.Table(
 
 fig2.update_layout(width=500, height=300)
 fig2.show()
+
+#
+## using Pandas Dataframe
+#
+
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_usa_states.csv')
+
+fig3 = go.Figure(data=[go.Table(
+    header=dict(values=list(df.columns),
+                fill_color='paleturquoise',
+                align='left'),
+    cells=dict(values=[df.Rank, df.State, df.Postal, df.Population],
+               fill_color='lavender',
+               align='left'))
+])
+
+fig3.show()
