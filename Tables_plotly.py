d3c12ad0f420dff3d8a0d4afdd0546b8a5c6b317
piokowalski <piokowalski@onet.pl>
2019-08-18 15:20:04 +0200

    Changing size of row and columns

--- Tables_plotly.py
+++ Tables_plotly.py
@@ -42,3 +42,38 @@ fig3 = go.Figure(data=[go.Table(
 ])
 
 fig3.show()
+
+#
+## Changing size of row and columns
+#
+
+values = [['Salaries', 'Office', 'Merchandise', 'Legal', '<b><br>Expenses</b>'], #1st col
+  ["Lorem ipsum dolor sit amet, tollit discere inermis pri ut. Eos ea iusto timeam, an prima laboramus vim. Id usu aeterno adversarium, summo mollis timeam vel ad",
+  "Lorem ipsum dolor sit amet, tollit discere inermis pri ut. Eos ea iusto timeam, an prima laboramus vim. Id usu aeterno adversarium, summo mollis timeam vel ad",
+  "Lorem ipsum dolor sit amet, tollit discere inermis pri ut. Eos ea iusto timeam, an prima laboramus vim. Id usu aeterno adversarium, summo mollis timeam vel ad",
+  "Lorem ipsum dolor sit amet, tollit discere inermis pri ut. Eos ea iusto timeam, an prima laboramus vim. Id usu aeterno adversarium, summo mollis timeam vel ad",
+  "Lorem ipsum dolor sit amet, tollit discere inermis pri ut. Eos ea iusto timeam, an prima laboramus vim. Id usu aeterno adversarium, summo mollis timeam vel ad"]]
+
+
+fig4 = go.Figure(data=[go.Table(
+  columnorder = [1,2],
+  columnwidth = [80,400],
+  header = dict(
+    values = [['<b>EXPENSES</b><br>as of July 2017'],
+                  ['<b>DESCRIPTION</b>']],
+    line_color='darkslategray',
+    fill_color='royalblue',
+    align=['left','center'],
+    font=dict(color='white', size=12),
+    height=40
+  ),
+  cells=dict(
+    values=values,
+    line_color='darkslategray',
+    fill=dict(color=['paleturquoise', 'white']),
+    align=['left', 'center'],
+    font_size=12,
+    height=30)
+    )
+])
+fig4.show()
