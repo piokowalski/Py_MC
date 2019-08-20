e8147649c26b9770ce4e6bcf2430464e79f3969c
piokowalski <piokowalski@onet.pl>
2019-08-20 23:40:30 +0200

    Figures charts practice - basic figure displaying itself

--- Charts_Figures1.py
+++ Charts_Figures1.py
@@ -9,3 +9,9 @@ fig1 = go.Figure(
     layout_title_text="A Figure Displayed with fig.show()"
 )
 fig1.show()
+# Displaying yourself
+fig2 = go.Figure(
+    data=[go.Bar(y=[2, 1, 3])],
+    layout_title_text="A Figure Displaying Itself"
+)
+fig2.show()
