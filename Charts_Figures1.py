03f0da9db441a644711724e967d7a13e94b4546c
piokowalski <piokowalski@onet.pl>
2019-08-20 23:42:27 +0200

    Figures charts practice - PNG renderer (some issues)

--- Charts_Figures1.py
+++ Charts_Figures1.py
@@ -15,3 +15,13 @@ fig2 = go.Figure(
     layout_title_text="A Figure Displaying Itself"
 )
 fig2.show()
+
+#
+## Using PNG renderer
+#
+
+fig3 = go.Figure(
+    data=[go.Bar(y=[2, 1, 3])],
+    layout_title_text="A Figure Displayed with the 'png' Renderer"
+)
+fig3.show(renderer="png")
