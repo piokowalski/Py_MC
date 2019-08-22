5cc2d89a3c7c867d8f0e479593ebebbb144a9ebd
piokowalski <piokowalski@onet.pl>
2019-08-22 23:06:18 +0200

    SQL - creating db with Python

--- SQL_sqlite3.py
+++ SQL_sqlite3.py
@@ -1,3 +1,29 @@
 import sqlite3
 
 connection = sqlite3.connect("myTable.db")
+crsr = connection.cursor()
+
+sql_command = """CREATE TABLE emp (  
+staff_number INTEGER PRIMARY KEY,  
+fname VARCHAR(20),  
+lname VARCHAR(30),  
+gender CHAR(1),  
+joining DATE);"""
+
+# execute the statement
+crsr.execute(sql_command)
+
+# SQL command to insert the data in the table
+sql_command = """INSERT INTO emp VALUES (23, "Rishabh", "Bansal", "M", "2014-03-28");"""
+crsr.execute(sql_command)
+
+# another SQL command to insert the data in the table
+sql_command = """INSERT INTO emp VALUES (1, "Bill", "Gates", "M", "1980-10-28");"""
+crsr.execute(sql_command)
+
+# To save the changes in the files. Never skip this.
+# If we skip this, nothing will be saved in the database.
+connection.commit()
+
+# close the connection
+connection.close()
